56af16b9b1414e6263beb5b1fe189d37a06d68e9
Juan Carlos Coruña <oggers@gmail.com>
2015-03-09 11:35:18 +0100

    Fix bed sensor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -124,10 +124,10 @@ Here are some standard links for getting your machine calibrated:
 //     #define DUMMY_THERMISTOR_999_VALUE 100
 
 #define TEMP_SENSOR_0 5  // e3d v6
-#define TEMP_SENSOR_1 1  // bed
+#define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
-#define TEMP_SENSOR_BED 0
+#define TEMP_SENSOR_BED 1
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
 //#define TEMP_SENSOR_1_AS_REDUNDANT
